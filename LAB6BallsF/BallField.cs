825a3af3dc09701be1b87f417f6895cde0bd08af
ilyapavych <ipavych@gmail.com>
2023-11-10 14:42:26 -0800

    Part 1

---
+++ LAB6BallsF/BallField.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LAB6BallsF
+{
+	public class BallField : IDrawable 
+	{
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            // Drawing code goes here
+        }
+    }
+}
+
