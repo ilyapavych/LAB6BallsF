5019e49a93c18de752d80462d6e0a4d932859006
ilyapavych <ipavych@gmail.com>
2023-11-10 15:57:10 -0800

    Part3(but balls dont move()

--- LAB6BallsF/BallField.cs
+++ LAB6BallsF/BallField.cs
@@ -3,12 +3,17 @@ namespace LAB6BallsF;
 
 public class BallField : IDrawable 
 {
+    public int position = 0;
+
     public void Draw(ICanvas canvas, RectF dirtyRect)
     {
+        // Drawing code goes here
         canvas.StrokeColor = Colors.Red;
         canvas.StrokeSize = 4;
-        canvas.DrawEllipse(10, 10, 100, 100);
-        // Drawing code goes here
+        canvas.DrawEllipse(10 + position++, 10 + position++, 100, 100);
+        //
+        canvas.FillColor = Colors.Red;
+        canvas.FillEllipse(200 + position++, 200 + position++, 150, 50);
     }
 }
 
--- LAB6BallsF/MainPage.xaml.cs
+++ LAB6BallsF/MainPage.xaml.cs
@@ -1,23 +1,33 @@
-﻿namespace LAB6BallsF;
+﻿using System.Timers;
+using Timer = System.Timers.Timer;
+
+namespace LAB6BallsF;
 
 public partial class MainPage : ContentPage
 {
+	private double milliSecondsPerFrame = 30; // this is the number of ms
+	public Timer timer; // this is the timer
+
 	public MainPage()
 	{
 		InitializeComponent();
 	}
-	void ContentPage_Loaded(System.Object sender, System.EventArgs e)
+
+	private void ContentPage_Loaded(Object sender, EventArgs e)
 	{
-		drawBall();
+		timer = new Timer();
+		timer.Interval = milliSecondsPerFrame;
+		timer.Elapsed += new ElapsedEventHandler(updateBallField);
+		timer.Start();
 	}
-	private void drawBall()
+
+	private void updateBallField(object sender, ElapsedEventArgs e)
 	{
-		var graphicsView = this.BallGraphicsView;
-        // maybe dontor need Drawable
-        //var ballFieldDrawable = (BallField)graphicsView.Drawable;
-		//ballFieldDrawable.
-
-    }
+            var graphicsView = this.BallGraphicsView;
+            // maybe dontor need Drawable
+            //var ballFieldDrawable = (BallField)graphicsView.Drawable;
+            //ballFieldDrawable.
+            graphicsView.Invalidate();
+        }
 }
-
 
