24bb9de9aa4919dec0d4d45d517938be8b463940
ilyapavych <ipavych@gmail.com>
2023-11-10 14:59:56 -0800

    Part 2

--- LAB6BallsF/BallField.cs
+++ LAB6BallsF/BallField.cs
@@ -1,13 +1,14 @@
 ﻿using System;
+namespace LAB6BallsF;
 
-namespace LAB6BallsF
+public class BallField : IDrawable 
 {
-	public class BallField : IDrawable 
-	{
-        public void Draw(ICanvas canvas, RectF dirtyRect)
-        {
-            // Drawing code goes here
-        }
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        canvas.StrokeColor = Colors.Red;
+        canvas.StrokeSize = 4;
+        canvas.DrawEllipse(10, 10, 100, 100);
+        // Drawing code goes here
     }
 }
 
--- LAB6BallsF/MainPage.xaml.cs
+++ LAB6BallsF/MainPage.xaml.cs
@@ -6,6 +6,18 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+	void ContentPage_Loaded(System.Object sender, System.EventArgs e)
+	{
+		drawBall();
+	}
+	private void drawBall()
+	{
+		var graphicsView = this.BallGraphicsView;
+        // maybe dontor need Drawable
+        //var ballFieldDrawable = (BallField)graphicsView.Drawable;
+		//ballFieldDrawable.
+
+    }
 }
 
 
